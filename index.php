9f7d4022f85d5bab92411ccc40e37687dd7fac83
NuengDunk <nueng.dunk@gmail.com>
2018-11-29 00:34:17 +0700

    test send message

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ use \LINE\LINEBot\HTTPClient\CurlhttpClient;
 use \LINE\LINEBot;
 use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
 
-$channel_token = 'glWWbmCy8qeIGPfdzNIz1Ydt1QUgJig5ZMS85R+5FCDSZtqUqPktppGlzEsPhowroCa77gdZR5Cyz8NtGpeQ1WhXuR4PMhl37MZ7CM/ScePTBISg3tahx5e9Xe8NZHsqVftG7FOyiwS6juZz4xPVtQdB04t89/1O/w1cDnyilFU=
+$channel_token = 'jw+uc94ihf67vskOBknb1q0jHjXvYzpN4XlgncMCuSXhZ6LAD1Aefklx9m4FKDMKoCa77gdZR5Cyz8NtGpeQ1WhXuR4PMhl37MZ7CM/SceOFLdtBB/CZygd/ZLf54BqdgV45NeLwlKwJ6MiFuoJgjQdB04t89/1O/w1cDnyilFU=
 ';
 $channel_secret = 'e616ea5c4234eec43f2c453319c9cc0c';
 
@@ -29,11 +29,11 @@ if(!is_null($events['events'])){
 					$respMessage = 'Hello, your message is '. $event['message']['text']; 
 					
 				break;
-				case 'image':
+				/*case 'image':
 					$messageID = $event['message']['id']; 
 					$respMessage = 'Hello, your image ID is '. $messageID;
 					
-				break;
+				break;*/
 				
 					
 			}
@@ -44,6 +44,7 @@ if(!is_null($events['events'])){
 		}
 	}
 }
+/*
 //Insert to DB
 $params = array( 
 	'type' => $event['type'],
@@ -55,5 +56,6 @@ $params = array(
 	'replytoken' => $event['replyToken']
 ); 
 $statement = $connection->prepare('INSERT INTO chatlogs (type, userID, time, message_id, message_type, message_text, replytoken) VALUES (:type, :userID, :time, :message_id, :message_type, :message_text, :repltoken)'); 
-$statement->execute($params);
+$statement->execute($params);*/
+?>
 
