65cb01d831a9baeca5fc90d13e50637d3d5251cc
NuengDunk <nueng.dunk@gmail.com>
2018-11-29 14:21:08 +0700

    Fix connection to db #8

--- inc/connect.php
+++ inc/connect.php
@@ -5,8 +5,8 @@ $user = 'nuengdunk';
 $pass = '21092528'; 
 $port = '5432';
 $option = '--client_encoding=UTF8';
-//$connection = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$pass option=$option");
-$connection = new PDO("pgsql:host=$host;port=$port;dbname=$dbname", $user, $pass);
+//$connection = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$pass options=$option");
+$connection = new PDO("pgsql:host=$host port=$port user=$user password=$pass dbname=$dbname options=$option");
 if (!$connection) {
   echo "An error occurred.\n";
   exit;
