bfae65e8fb2a249b69b2bf29ddb440f3998bb9da
NuengDunk <nueng.dunk@gmail.com>
2018-11-29 14:17:03 +0700

    Insert to DB #7

--- inc/connect.php
+++ inc/connect.php
@@ -5,8 +5,8 @@ $user = 'nuengdunk';
 $pass = '21092528'; 
 $port = '5432';
 $option = '--client_encoding=UTF8';
-$connection = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$pass option=$option");
-//$connection = new PDO("pgsql:host=$host;port=$port;dbname=$dbname", $user, $pass);
+//$connection = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$pass option=$option");
+$connection = new PDO("pgsql:host=$host;port=$port;dbname=$dbname", $user, $pass);
 if (!$connection) {
   echo "An error occurred.\n";
   exit;
