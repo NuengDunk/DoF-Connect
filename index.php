9b1d50a953a7987fe4d1cc920b080ed617aab2bf
NuengDunk <nueng.dunk@gmail.com>
2018-11-29 12:31:10 +0700

    update index.php

--- index.php
+++ index.php
@@ -1,36 +1,3 @@
 <?php 
-require('vendor/autoload.php'); 
-require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
-require('inc/connect.php');
-require('inc/setting.php');
 
-// Namespace 
-use \LINE\LINEBot\HTTPClient\CurlHTTPClient; 
-use \LINE\LINEBot; 
-use \LINE\LINEBot\MessageBuilder\TextMessageBuilder; 
-
-// Get message from Line API 
-$content = file_get_contents('php://input'); 
-$events = json_decode($content, true); 
-if (!is_null($events['events'])) { 
-	// Loop through each event 
-	foreach ($events['events'] as $event) 
-	{ 
-		// Line API send a lot of event type, we interested in message only. 
-		if ($event['type'] == 'message') { 
-			switch($event['message']['type']) { 
-				case 'text': 
-					// Get replyToken 
-					$replyToken = $event['replyToken']; 
-					// Reply message 
-					$respMessage = 'Hello, your message is '. $event['message']['text']; 
-					$httpClient = new CurlHTTPClient($channel_token); 
-					$bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret)); 
-					$textMessageBuilder = new TextMessageBuilder($respMessage); 
-					$response = $bot->replyMessage($replyToken, $textMessageBuilder); 
-				break; 
-			} 
-		} 
-	} 
-}
 echo "Hello LINE BOT";
