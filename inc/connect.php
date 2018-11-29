114bf3f8c8cc0e2ec40418778c8c3a212a194a93
NuengDunk <nueng.dunk@gmail.com>
2018-11-29 15:06:02 +0700

    Insert to DB local by no-ip #2

--- inc/connect.php
+++ inc/connect.php
@@ -6,7 +6,7 @@ $pass = '21092528';
 $port = '5432';
 $option = '--client_encoding=UTF8';
 //$connection = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$pass options=$option");
-$connection = new PDO("pgsql:host=$host port=$port user=$user password=$pass dbname=$dbname options=$option");
+$connection = new PDO("pgsql:host=$host;dbname=$dbname", $user, $pass);
 if (!$connection) {
   echo "An error occurred.\n";
   exit;
