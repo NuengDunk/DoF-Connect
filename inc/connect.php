7d075d625ff322b5d6545675ec0952b237ed5961
NuengDunk <nueng.dunk@gmail.com>
2018-11-29 15:03:05 +0700

    Fix connection to db to local by no-ip

--- inc/connect.php
+++ inc/connect.php
@@ -1,12 +1,12 @@
 <?php
-$host = '127.0.0.1'; 
+$host = 'dofdb.ddns.net'; 
 $dbname = 'dof_connect'; 
 $user = 'nuengdunk'; 
-$pass = '21092528'; 
+$pass = '21092528';
 $port = '5432';
 $option = '--client_encoding=UTF8';
 //$connection = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$pass options=$option");
-$connection = new PDO("pgsql:host=$host; port=$port; user=$user; password=$pass; dbname=$dbname; options=$option");
+$connection = new PDO("pgsql:host=$host port=$port user=$user password=$pass dbname=$dbname options=$option");
 if (!$connection) {
   echo "An error occurred.\n";
   exit;
