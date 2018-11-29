2c3b0bab3860a9f797fde142c688d36343628971
NuengDunk <nueng.dunk@gmail.com>
2018-11-29 14:10:12 +0700

    Insert to DB #4

--- inc/connect.php
+++ inc/connect.php
@@ -4,9 +4,7 @@ $dbname = 'dof_connect';
 $user = 'nuengdunk'; 
 $pass = '21092528'; 
 $option = '--client_encoding=UTF8';
-//$connection = new PDO("pgsql:host=$host;dbname=$dbname, $user, $pass");
-$connection = pg_connect("host=$host port=5432 dbname=$dbname user=$user password=$pass option=$option");
-//$connection = new PDO("pgsql:host=$host;dbname=$dbname", $user, $pass);
+$connection = new PDO("pgsql:host=$host;port=$port;dbname=$dbname;option=$option", $user, $pass);
 if (!$connection) {
   echo "An error occurred.\n";
   exit;
