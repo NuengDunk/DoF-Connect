e6e9239f57ee31fbca76c04aa6e43d6c38346672
NuengDunk <nueng.dunk@gmail.com>
2018-11-30 01:18:13 +0700

    Change to mariadb10

--- inc/connect.php
+++ inc/connect.php
@@ -1,12 +1,13 @@
 <?php
-$host = 'ocdns3.onecool.co.th'; 
+$host = '27.254.220.7'; 
 $dbname = 'dof_connect'; 
 $user = 'nuengdunk'; 
-$pass = '21092528';
+$pass = 'Tobpt21092528';
 $port = '5432';
 $option = '--client_encoding=UTF8';
 //$connection = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$pass options=$option");
-$connection = new PDO("pgsql:host=$host;dbname=$dbname", $user, $pass);
+//$connection = new PDO("pgsql:host=$host;dbname=$dbname", $user, $pass);
+$connection = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
 if (!$connection) {
   echo "An error occurred.\n";
   exit;
