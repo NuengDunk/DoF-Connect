d714783eb45e135561bf75c89ff0c4d7a17443f8
NuengDunk <nueng.dunk@gmail.com>
2018-11-29 16:15:46 +0700

    Change DB to heroku postgresql

--- inc/connect.php
+++ inc/connect.php
@@ -1,8 +1,8 @@
 <?php
-$host = 'dofdb.ddns.net'; 
-$dbname = 'dof_connect'; 
-$user = 'nuengdunk'; 
-$pass = '21092528';
+$host = 'ec2-54-243-150-10.compute-1.amazonaws.com'; 
+$dbname = 'd7u6khk8sokcak'; 
+$user = 'idiwrkfrvcfnjp'; 
+$pass = '35274bd0032f592a8ba7acfd4cef0bd3fdd2b230ae3c460dc457b32aed25eca5';
 $port = '5432';
 $option = '--client_encoding=UTF8';
 //$connection = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$pass options=$option");
