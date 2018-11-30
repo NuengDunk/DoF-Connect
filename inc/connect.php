09d7079be758533f95005e57efaab6e020df2941
NuengDunk <nueng.dunk@gmail.com>
2018-11-30 14:08:29 +0700

    initial...

--- inc/connect.php
+++ inc/connect.php
@@ -1,10 +1,10 @@
 <?php
 
-/*$host = '27.254.220.7'; 
+$host = '27.254.220.7'; 
 $dbname = 'dof_connect'; 
 $user = 'nuengdunk'; 
 $pass = 'Tobpt21092528';
-$port = '3306';*/
+$port = '3306';
 /*$host = 'ocdns3.onecool.co.th';
 $dbname = 'dof_connect';
 $user = 'nuengdunk';
