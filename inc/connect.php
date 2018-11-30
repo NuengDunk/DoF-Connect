9952512ada6e1478811245a9221975e85456d431
NuengDunk <nueng.dunk@gmail.com>
2018-11-30 14:06:56 +0700

    initial...

--- inc/connect.php
+++ inc/connect.php
@@ -5,15 +5,15 @@ $dbname = 'dof_connect';
 $user = 'nuengdunk'; 
 $pass = 'Tobpt21092528';
 $port = '3306';*/
-$host = 'ocdns3.onecool.co.th';
+/*$host = 'ocdns3.onecool.co.th';
 $dbname = 'dof_connect';
 $user = 'nuengdunk';
 $pass = '21092528';
-$port = '5432';
+$port = '5432';*/
 $option = '--client_encoding=UTF8';
 //$connection = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$pass options=$option");
-//$connection = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
-$connection = new PDO("pgsql:host=$host;dbname=$dbname", $user, $pass);
+$connection = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+//$connection = new PDO("pgsql:host=$host;dbname=$dbname", $user, $pass);
 if (!$connection) {
   echo "An error occurred.\n";
   exit;
