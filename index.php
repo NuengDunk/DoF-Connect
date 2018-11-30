a75cd43234a4a289bb36ffedcfc048c129b26afd
NuengDunk <nueng.dunk@gmail.com>
2018-11-30 19:23:48 +0700

    Verify Line bot

--- index.php
+++ index.php
@@ -36,13 +36,38 @@ if(!is_null($events['events'])){
 				$statement->bindParam(':userID',$event['source']['userId']);
 				$statement->bindParam(':time',$appointments[0]);
 				$statement->bindParam(':time',$appointments[1]);
-				$result = $statement->excute($params);
+				$result = $statement->excute($statement);
 				
 				$respMessage = 'Your appointment has saved';			
 			}else{
 				$respMessage = 'You can send appointment like this "12:00, Run always"';
 			}
 			
+			   // Build message to reply back
+		   $messages = [
+			'type' => 'text',
+			'text' => $respMessage
+		   ];
+
+		   // Make a POST Request to Messaging API to reply to sender
+		   $url = 'https://api.line.me/v2/bot/message/reply';
+		   $data = [
+			'replyToken' => $replyToken,
+			'messages' => [$messages],
+		   ];
+		   $post = json_encode($data);
+		   $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $channel_token);
+
+		   $ch = curl_init($url);
+		   curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+		   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		   curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+		   curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+		   curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+		   $result = curl_exec($ch);
+		   curl_close($ch);
+
+		   echo $result . "\r\n";
 			$httpClient = newCurlHTTPClient($channel_token);
 			$bot = newLineBot($httpClient,array('chanelSecret' => $channel_secret));
 			
