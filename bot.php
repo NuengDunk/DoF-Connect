8723f458b8c5237ad13b9076fd743b697b59ce56
NuengDunk <nueng.dunk@gmail.com>
2018-11-30 20:51:50 +0700

    change exec to query

--- bot.php
+++ bot.php
@@ -31,7 +31,7 @@ if (!is_null($events['events'])) {
 				/*$statement->bindParam(':userID',$event['source']['userId']);
 				$statement->bindParam(':time',$appointments[0]);
 				$statement->bindParam(':time',$appointments[1]);*/
-				$result = $connection->exec($statement);
+				$result = $connection->query($statement);
 		  		if($result){
 					$respMessage = 'ข้อความของคุณได้ถูกบันทึกไว้แล้ว';						
 				}else{
