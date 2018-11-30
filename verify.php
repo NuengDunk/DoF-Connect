b82297e91d49e8c55a46e703228e81100863e98b
NuengDunk <nueng.dunk@gmail.com>
2018-11-30 17:59:33 +0700

    Verify Line bot

--- verify.php
+++ verify.php
@@ -1,10 +1,10 @@
 <?php
-$access_token = 'F05gp6P3RhMJNxfEMd2q8+eT1EDydapR5e+WlMd4PtD2TrqyP7tVSJRLfMlgUCHzoCa77gdZR5Cyz8NtGpeQ1WhXuR4PMhl37MZ7CM/SceNzKp7FB9FktKc95cId1yoUvJYR02KB85texe9ZzQ3Q1gdB04t89/1O/w1cDnyilFU=';
+include('./inc/setting.php');
 
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
-$headers = array('Authorization: Bearer ' . $access_token);
+$headers = array('Authorization: Bearer ' . $channel_token);
 
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
