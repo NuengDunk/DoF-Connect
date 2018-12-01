14774868c35ae90c246996442c7bf55d42b777a7
NuengDunk <nueng.dunk@gmail.com>
2018-12-01 23:12:16 +0700

    Try Mysqil

--- inc/connect.php
+++ inc/connect.php
@@ -12,9 +12,11 @@ $pass = '21092528';
 $port = '5432';*/
 $option = '--client_encoding=UTF8';
 //$connection = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$pass options=$option");
-$connection = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
-$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+/*$connection = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);*/
 //$connection = new PDO("pgsql:host=$host;dbname=$dbname", $user, $pass);
+
+$connection = new mysqli($host, $user, $pass, $dbname);
 if (!$connection) {
   echo "An error occurred.\n";
   exit;
